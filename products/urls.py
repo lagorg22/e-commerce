3209a5fe73c52cffed844473c80127abd5a5bc9b
lagorg22 <lagorg22@freeuni.edu.ge>
2025-03-27 19:30:07 +0400

    added urls for update and delete methods

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.category_list, name='category_list'),  
     path('list/', views.product_list, name='product_list'),  
-    path('add/', views.add_product, name='add_product'), 
+    path('add/', views.add_product, name='add_product'),
+    path('products/<int:product_id>/', views.update_product, name='update-product'),
+    path('products/<int:product_id>/delete/', views.delete_product, name='delete-product'), 
 ]
