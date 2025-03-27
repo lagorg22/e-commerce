ff8eba63993c8f7e140bfe6bf23b6c575641aa04
lagorg22 <lagorg22@freeuni.edu.ge>
2025-03-28 03:17:42 +0400

    add: implement API root endpoint to provide a centralized view of available endpoints for users, products, cart, and orders

--- MyShop/urls.py
+++ MyShop/urls.py
@@ -22,6 +22,42 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    """
+    API root showing all available endpoints.
+    """
+    return Response({
+        'users': {
+            'register': reverse('register', request=request, format=format),
+            'register_admin': reverse('register-admin', request=request, format=format),
+            'login': reverse('login', request=request, format=format),
+            'profile': reverse('profile', request=request, format=format),
+            'logout': reverse('logout', request=request, format=format),
+            'change_password': reverse('change-password', request=request, format=format),
+            'delete_account': reverse('delete-account', request=request, format=format),
+            'deposit': reverse('deposit-funds', request=request, format=format),
+            'transactions': reverse('transaction-history', request=request, format=format),
+        },
+        'products': {
+            'categories': reverse('category_list', request=request, format=format),
+            'list': reverse('product_list', request=request, format=format),
+            'add': reverse('add_product', request=request, format=format),
+        },
+        'cart': {
+            'view': reverse('view-cart', request=request, format=format),
+            'add': reverse('add-to-cart', request=request, format=format),
+        },
+        'orders': {
+            'list': reverse('order-list', request=request, format=format),
+            'create': reverse('create-order', request=request, format=format),
+        },
+        'docs': reverse('schema-swagger', request=request, format=format),
+    })
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -38,6 +74,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root, name='api-root'),
     path('products/', include('products.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
     path('users/', include('users.urls')),
