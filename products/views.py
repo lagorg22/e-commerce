62e36330a9c885add2ee95b844d6dca616857e72
lagorg22 <lagorg22@freeuni.edu.ge>
2025-03-27 15:51:58 +0400

    implement: basic views

---
+++ products/views.py
@@ -0,0 +1,29 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, parser_classes
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+from rest_framework import status
+
+
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def add_product(request):
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
